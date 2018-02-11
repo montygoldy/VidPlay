9b0e06b69aa3746eb120f5e51e39e5ac23df9731
montygoldy <montygoldy2006@gmail.com>
2018-02-10 22:00:29 -0500

    Added lodash to debounce the search result

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,10 +8,15 @@ class SearchBar extends Component{
     this.state = {term: ''};
   }
 
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
   render(){
     return (
       <div>
-        <input value = {this.state.term} onChange = {event => this.setState({ term: event.target.value})} />
+        <input value = {this.state.term} onChange = {event => this.onInputChange(event.target.value)} />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+
 
 const API_KEY = "AIzaSyDIBlD4mrQnRzayZJT2ckZHZigh85zf6kA";
 
@@ -12,17 +14,20 @@ class App extends Component{
     super(props);
 
     this.state = { videos: [], selectedVideo: null };
+    this.videoSearch('surfboards');
+  }
 
-    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+  videoSearch(term){
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
       this.setState({ videos: videos, selectedVideo: videos[0] });
     })
-
   }
 
   render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return(
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} onVideoSelect={selectedVideo => this.setState({selectedVideo})}/>
       </div>
