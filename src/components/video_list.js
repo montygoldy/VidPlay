1a18c1b2d881abe7d1e3a7c6235c361b97c446f7
montygoldy <montygoldy2006@gmail.com>
2018-02-10 20:42:36 -0500

    Added videos to video list item

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
-const VideoList = () => {
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} key={video.etag}/>
+  })
   return(
     <ul className="col-md-4 list-group">
-      <li></li>
+      {videoItems}
     </ul>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY = "AIzaSyDIBlD4mrQnRzayZJT2ckZHZigh85zf6kA";
 
@@ -21,6 +22,7 @@ class App extends Component{
     return(
       <div>
         <SearchBar />
+        <VideoList videos = {this.state.videos} />
       </div>
     )
   }
