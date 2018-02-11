247040a8369b52d975f7acc7b21b9efcd88ac530
montygoldy <montygoldy2006@gmail.com>
2018-02-10 20:10:20 -0500

    Added search to the default state of the app

---
+++ src/components/video_list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoList = () => {
+  return(
+    <ul className="col-md-4 list-group">
+      <li></li>
+    </ul>
+  )
+}
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = "AIzaSyDIBlD4mrQnRzayZJT2ckZHZigh85zf6kA";
 
-const App = () => {
-  return <div>Hi!</div>;
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    })
+
+  }
+
+  render(){
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
+
 
 ReactDOM.render(<App />, document.querySelector('#container'));
